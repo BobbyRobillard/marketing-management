1106617cb4417aab451854bde9dbd31a13ccee2f
Bobby Robillard <robillard123@gmail.com>
2019-02-10 21:58:22 -0800

    Added initial models

--- administration/models.py
+++ administration/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Project(models.Model):
+    name = models.CharField(max_length=100)
+    recieved = models.DecimalField(max_digits=4, decimal_places=2, default=0)
+
+class StoreAuthentication(models.Model):
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
--- posting/models.py
+++ posting/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+name_length = 100
+
+class Topic(models.Model):
+    name = models.CharField(max_length=name_length)
+    project = models.ForeignKey('administration.Project', on_delete=models.CASCADE)
+
+class Post(models.Model):
+    name = models.CharField(max_length=name_length)
+    project = models.ForeignKey('administration.Project', on_delete=models.CASCADE)
+    url = models.URLField()
+    posted = models.DateField()
+    topic = models.ForeignKey(Topic, on_delete=models.SET_NULL, null=True)
+    earned = models.DecimalField(max_digits=10, decimal_places=2, default=0)
