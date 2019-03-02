3fe614791afbdba557e70f7ffed17e1c9167fc44
Bobby Robillard <robillard123@gmail.com>
2019-03-01 19:36:18 -0800

    Able to add coupon from project details page

--- administration/models.py
+++ administration/models.py
@@ -7,6 +7,7 @@ from woocommerce import API
 class Project(models.Model):
     name = models.CharField(max_length=100, unique=True)
     percent_received = models.DecimalField(max_digits=4, decimal_places=2, default=0)
+    add_coupon_url = models.URLField()
 
     def __str__(self):
         return self.name
