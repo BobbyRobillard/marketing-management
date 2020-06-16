255332a760c5d2e4231a0cc0bb98c51242353ca6
Bobby Robillard <robillard123@gmail.com>
2020-06-16 10:02:58 -0400

    Polls can now be deleted

--- voting/urls.py
+++ voting/urls.py
@@ -10,4 +10,5 @@ urlpatterns = [
     # General Page Views
     url(r"^$", views.homepage_view, name="homepage"),
     url(r"^add-poll$", views.add_poll, name="add_poll"),
+    url(r"^delete-poll/(?P<pk>\d+)/$", views.DeletePollView.as_view(), name="delete_poll"),
 ]
--- voting/views.py
+++ voting/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render, redirect
+
+from django.contrib import messages
 from django.contrib.auth.decorators import login_required
+
 from django.utils.decorators import method_decorator
 
 from django.views.generic.edit import DeleteView, UpdateView, CreateView
@@ -31,3 +34,19 @@ def add_poll(request):
             context['form'] = form
             context['polls'] = Poll.objects.all()
     return render(request, 'voting/voting.html', context)
+
+
+@method_decorator(login_required, name="dispatch")
+class DeletePollView(DeleteView):
+    model = Poll
+    success_url = "/voting"
+
+    def get_context_data(self, **kwargs):
+        return get_class_based_default_context(
+            super().get_context_data(**kwargs),
+            self.request.user
+        )
+
+    def delete(self, *args, **kwargs):
+        messages.success(self.request, "Poll deleted!")
+        return super(DeletePollView, self).delete(*args, **kwargs)
