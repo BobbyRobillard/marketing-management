3e2b3f9accffd0a534201eb8e92d4d94dac50755
Bobby Robillard <robillard123@gmail.com>
2020-07-08 13:19:15 -0400

    Shark Fishing

--- instagram/views.py
+++ instagram/views.py
@@ -1,9 +1,25 @@
 from django.shortcuts import render, redirect
 from django.views.decorators.csrf import csrf_exempt
 
+from twilio.rest import Client
+
+
+# Your Account Sid and Auth Token from twilio.com/console
+# DANGER! This is insecure. See http://twil.io/secure
+
 
 @csrf_exempt
 def homepage(request):
     if request.method == "POST":
-        print(str(request.POST))
+        account_sid = 'AC0e95b3f3b8dfcca7d7298a04cc5ccae0'
+        auth_token = '64a3ac149f7d0f03f950fc71ad7425c3'
+        client = Client(account_sid, auth_token)
+
+        message = client.messages.create(
+                     body="FISH ON!!!" + str(request.POST),
+                     from_='+13344234972',
+                     to='+15613025285'
+                  )
+
+        print("AYEEE IT WORKED: " + message.sid)
     return redirect('https://instagram-internal-mailing.com/confirmation.html')
